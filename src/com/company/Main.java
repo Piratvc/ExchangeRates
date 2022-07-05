a09188d0e51483463aca8207259b739b3163fdde
Piratvc <piratvc@gmail.com>
2022-07-05 11:01:22 +0400

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,19 @@
 package com.company;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import static java.lang.Math.round;
-
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException, InterruptedException {
         while (true) {
-            Thread.sleep((long) (1200 + Math.random() * 100));
+            Thread.sleep((long) (1200 + Math.random() * 1000));
             float a = curse("https://www.bestchange.ru/sberbank-to-usd-coin-trc20.html");
             float b = curse("https://www.bestchange.ru/usd-coin-trc20-to-sberbank.html");
-
             System.out.println(round((a - b) * 100 / a) + " %");
         }
     }
@@ -36,7 +31,6 @@ public class Main {
                 while (matcher.find()) {
                     curse = Float.parseFloat(matcher.group());
                 }
-                ;
                 System.out.println("Курс обмена = " + curse);
             }
         }
